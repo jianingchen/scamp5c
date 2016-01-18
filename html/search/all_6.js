0d793acd95dc875cf544dba7ac893580784e0338
Jianing Chen <jianing.chen@manchester.ac.uk>
2016-01-18 19:00:13 +0000

    update

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['io_5faout',['io_aout',['../a00027.html#a107c7e9708eb0d70dcec5af62623e8a2',1,'io_macros.h']]],
+  ['io_5fd2aout',['io_d2aout',['../a00027.html#ac9cd74d812c5c0d5fa82e23e2f70c093',1,'io_macros.h']]],
   ['io_5fmacros_2eh',['io_macros.h',['../a00027.html',1,'']]],
   ['io_5fslider',['io_slider',['../a00027.html#a42f84ed20a1f129fddd596b1d3b11920',1,'io_macros.h']]],
   ['ipu_5finstructions_2eh',['ipu_instructions.h',['../a00028.html',1,'']]],
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -2,11 +2,10 @@ var searchData=
 [
   ['f_5fabs',['F_abs',['../a00029.html#a3d48ea13aec9b954e72e1278e5c8d4ff',1,'pe_analog_macros.h']]],
   ['f_5fadd',['F_add',['../a00029.html#a710e6cc82629b382ae0e1d56d0bb0728',1,'pe_analog_macros.h']]],
-  ['f_5fcopy',['F_copy',['../a00029.html#a3d07cc65553a6a209f4fcd6f99b2de86',1,'pe_analog_macros.h']]],
   ['f_5fdiv',['F_div',['../a00029.html#a5563d04b8a9fefd392e7469cb8a3b6e8',1,'pe_analog_macros.h']]],
-  ['f_5fdump_5fpix',['F_dump_pix',['../a00029.html#a04e3cce40a0281d40767acd40ae716b6',1,'pe_analog_macros.h']]],
   ['f_5fin',['F_in',['../a00029.html#af4b265f9fc332d2884107a3261bc438e',1,'pe_analog_macros.h']]],
+  ['f_5fmov',['F_mov',['../a00029.html#a65f32d26dcc0ec3b1e0d36bf640f864c',1,'pe_analog_macros.h']]],
   ['f_5fneg',['F_neg',['../a00029.html#a58f90acd47320b46c7c58fc47bcb2662',1,'pe_analog_macros.h']]],
-  ['f_5frespix',['F_respix',['../a00029.html#aabf86f1775444687d180eb6b7c1d5782',1,'pe_analog_macros.h']]],
+  ['f_5frpix',['F_rpix',['../a00029.html#a56fafa1a1ef99a1b063f843680c11ff7',1,'F_rpix():&#160;pe_analog_macros.h'],['../a00029.html#aa3b42fcdea3fb550ca4fa31986dafbd8',1,'F_rpix(areg a1, areg a2):&#160;pe_analog_macros.h']]],
   ['f_5fsub',['F_sub',['../a00029.html#ab470f9361c60681bcc191fbb1b689c92',1,'pe_analog_macros.h']]]
 ];
