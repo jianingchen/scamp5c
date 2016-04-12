a8a49bf586f93fa82d2de4e1b30bf17891f4f1b3
Jianing Chen <j.n.chen@sheffield.ac.uk>
2016-04-12 15:24:09 +0100

    minor fix

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['ipu_5fglobal_5fsum',['ipu_global_sum',['../a00025.html#a025e51ee743e1ee759948408a4b4a20d',1,'ipu_instructions.h']]],
+  ['ipu_5fpixel_5fselect',['ipu_pixel_select',['../a00025.html#a7b45050f479fcf56fde68b6c543135f8',1,'ipu_instructions.h']]],
   ['ipu_5fpixel_5fset',['ipu_pixel_set',['../a00025.html#ad68fa867e21e5ecc4a60f44baa4aa1bc',1,'ipu_instructions.h']]],
   ['ipu_5fwait_5fframe_5ftrigger',['ipu_wait_frame_trigger',['../a00025.html#ae67c1f24e4bec58094a95fddb5282215',1,'ipu_instructions.h']]]
 ];
