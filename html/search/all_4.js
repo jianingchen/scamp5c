938a3dd1943bad6da69a682f09c276b3592fdfca
Jianing Chen <j.n.chen@sheffield.ac.uk>
2016-06-23 17:39:50 +0100

    update documentation for spi

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['handle',['handle',['../a00013.html',1,'']]],
   ['host_5fadd_5fdatasheet',['host_add_datasheet',['../a00024.html#a6624d82e3c8c59b0ddfa0156b58a2178',1,'host_and_sim.h']]],
   ['host_5fadd_5fdisplay',['host_add_display',['../a00024.html#a5b620814431ac260d2e287f5adcc739b',1,'host_add_display(string text):&#160;host_and_sim.h'],['../a00024.html#a283997b565c846fac5465023f0716dd4',1,'host_add_display(string text, string palette):&#160;host_and_sim.h']]],
-  ['host_5fadd_5fslider',['host_add_slider',['../a00024.html#a4b7cacf924ec2117ef530fd75811cc7c',1,'host_and_sim.h']]],
+  ['host_5fadd_5fslider',['host_add_slider',['../a00024.html#a8b77ad9083807166dcb6d62cd46e4511',1,'host_add_slider(string text, int min, int max, int init):&#160;host_and_sim.h'],['../a00024.html#a2e8b9a0e75a9a2a800b3c0596278dd19',1,'host_add_slider(string text, int min, int max, int init, int spi_name, int is_latched):&#160;host_and_sim.h']]],
   ['host_5fand_5fsim_2eh',['host_and_sim.h',['../a00024.html',1,'']]],
   ['host_5frecord_5fdisplay',['host_record_display',['../a00024.html#a35f31d53580e41d234e5106f1026190d',1,'host_and_sim.h']]],
   ['host_5fset_5fframe_5frate',['host_set_frame_rate',['../a00024.html#ac9b91fb61e745d20a1c2a47fcd54b1d5',1,'host_and_sim.h']]],
